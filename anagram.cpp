f9ef33a76028054e87a4786182927bd6756498b1
Kavya <kavya41999@gmail.com>
2020-09-02 22:41:03 +0530

    Update anagram.cpp

--- anagram.cpp
+++ anagram.cpp
@@ -7,32 +7,23 @@ int main()
 	
 	int p,j=0,mark=5,total=0;
 	char ch='y';
-	string ans,line,ansf,clue;
-	
+	string ans,line,ansf,clue;	
 	ifstream fih("anagramquestion.txt");
 	cout<<"Lets start the anagram game :)\n";
 	while(ch=='y')
 	{
 		getline(fih,line);
-	//	cout<<line;
 		p=line.find(":");
 		ansf=line.substr(0,p-1);
 		mark=5;
-		cout<<"Find the correct word from the letter: "<<ansf<<'\n';
-	
+		cout<<"Find the correct word from the letter: "<<ansf<<'\n';	
 		line=line.substr(p+1);
-	//	cout<<line;
-	//	i=p+1;
 		p=line.find(":");
 		ansf=line.substr(0,p);
-	//	cout<<ansf;
-			cin>>ans;
-	//	line=line.substr(p+1);
-	//	cout<<line;
+		cin>>ans;
 		while((ans!=ansf)&&(j<3)){
 			
 			j++;
-		//	i=p+1;
 			line=line.substr(p+1);
 			p=line.find(":");			
 			cout<<"\nWRONG ANSWER:(\n Try again\n"<<(3-j)<<" attempts left\n"<<"Hint:";
@@ -52,7 +43,6 @@ int main()
 			cout<<"TOTAL SCORE:"<<total<<'\n';
 		}
 		cout<<"Do you want to continue?y or n\n";
-		cin>>ch;
-			
+		cin>>ch;			
 	}
 }
